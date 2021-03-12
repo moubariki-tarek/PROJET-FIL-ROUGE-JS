875500de8478073caaa165dfcd44aabf2aa4bae5
abdelhamid1A <hamoda.abdo@outlook.fr>
2021-03-12 12:16:08 +0100

    aa

---
+++ Desktop/nd/server.js
@@ -0,0 +1,9 @@
+const express =require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('heroku test')
+})
+
+const port = 4000
+app.listen(port,()=>console.log('server run'))
