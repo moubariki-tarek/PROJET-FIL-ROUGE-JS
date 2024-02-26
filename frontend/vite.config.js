2b3c49e4cbb9dc0011d7570e76e1ac9e0ae14e1a
moubariki-tarek <tarek.brief@gmail.com>
2024-02-27 00:36:40 +0100

    remove rating

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5002",
-      "/uploads/": "http://localhost:5002",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
